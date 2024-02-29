419ce7f5ae607b3af5334b1d1e397a2c2a7c683e
Артем Апенко <lbapart@MacBook-Pro.local>
2024-02-29 18:38:11 +0100

    nginx works with compose

---
+++ srcs/requirements/wordpress/conf/wp-config.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * The base configuration for WordPress
+ *
+ * The wp-config.php creation script uses this file during the
+ * installation. You don't have to use the web site, you can
+ * copy this file to "wp-config.php" and fill in the values.
+ *
+ * This file contains the following configurations:
+ *
+ * * MySQL settings
+ * * Secret keys
+ * * Database table prefix
+ * * ABSPATH
+ *
+ * @link https://wordpress.org/support/article/editing-wp-config-php/
+ *
+ * @package WordPress
+ */
+define( 'DB_NAME', getenv('DBNAME') );
+define( 'DB_USER', getenv('MDBUSERNAME1') );
+define( 'DB_PASSWORD', getenv('MDBPASSWORD1') );
+
+define( 'DB_HOST', 'localhost' );
+
+/** Database Charset to use in creating database tables. */
+define( 'DB_CHARSET', 'utf8' );
+
+/** The Database Collate type. Don't change this if in doubt. */
+define( 'DB_COLLATE', '' );
+
+/* That's all, stop editing! Happy publishing. */
+
+/** Absolute path to the WordPress directory. */
+if ( ! defined( 'ABSPATH' ) ) {
+    define( 'ABSPATH', __DIR__ . '/' );
+}
+
+/** Sets up WordPress vars and included files. */
+require_once ABSPATH . 'wp-settings.php';
